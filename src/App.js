b9913fc468261af2e0eca9743d20de19802d0f63
sono <rhj080471@gmail.com>
2024-07-19 20:57:15 +0900

    Feat : firebase로 간단하게 구글 로그인 구현
    로그인, 로그아웃, 유저 정보 전달

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { createContext, useEffect, useState } from "react";
 import Router from "./component/Router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+
+export const userObjContext = createContext();
 
 function App() {
+  // 유저 정보 객체
+  const [userObj, setUserObj] = useState(null);
+
+  // 현재 사용자가 로그인 되어있는지 확인 후 유저 정보 가져오기
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      // 로그인이 되어있다면
+      if (user) {
+        setUserObj(user);
+      }
+    });
+  }, []);
   return (
-    <div>
-      <Router />
-    </div>
+    <>
+      <userObjContext.Provider value={userObj}>
+        <Router />
+      </userObjContext.Provider>
+    </>
   );
 }
 
--- src/page/login/Login.js
+++ src/page/login/Login.js
@@ -1,32 +1,43 @@
-import React from "react";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import React, { useContext } from "react";
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 import { auth } from "../../firebase";
+import { userObjContext } from "../../App";
 function Login() {
-  const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
+  // 유저 정보
+  const userObj = useContext(userObjContext);
+
+  // 구글 로그인
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+
+        // const token = credential.accessToken;
+
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // 로그아웃
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
 
   return (
     <div>
-      <button>구글로 로그인하기</button>
+      <button onClick={handleGoogleLogin}>구글로 로그인하기</button>
+      <button onClick={handleLogout}>로그아웃</button>
     </div>
   );
 }

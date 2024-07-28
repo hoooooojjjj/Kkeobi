ab35d1a0478f59685e4aa3cc403856fadd4bd590
sono <rhj080471@gmail.com>
2024-07-29 00:30:36 +0900

    Feat : 유저가 이미 로그인을 했었는지 여부 확인 로직 구현
    유저가 로그인 시 파이어스토어에 user 컬렉션에 저장하고 다시 로그인 하면 컬렉션에서 확인 후 로그인 유무 확인

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
 import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import { userObjContext } from "../App";
 import { GoogleLoginImg } from "../page/main/MainStyle";
 import { useNavigate } from "react-router-dom";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 function Login() {
   // 유저 정보
   const userObj = useContext(userObjContext);
@@ -14,15 +15,28 @@ function Login() {
   const handleGoogleLogin = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
-      .then((result) => {
-        // const credential = GoogleAuthProvider.credentialFromResult(result);
-
-        // const token = credential.accessToken;
-
+      .then(async (result) => {
         const user = result.user;
+
+        // 유저 정보 Firestore에서 확인
+        const userRef = doc(db, "users", user.uid);
+        const userSnap = await getDoc(userRef);
+
+        if (userSnap.exists()) {
+          console.log("이미 로그인한 유저입니다:", userSnap.data());
+        } else {
+          // Firestore에 유저 정보 저장
+          await setDoc(userRef, {
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+            accessToken: user.accessToken,
+          });
+          console.log("새로운 유저입니다. 유저 정보가 저장되었습니다.");
+        }
       })
       .catch((error) => {
-        console.log(error);
+        console.log("로그인 에러 : " + error);
       });
   };
 
@@ -31,6 +45,7 @@ function Login() {
     signOut(auth)
       .then(() => {
         // Sign-out successful.
+        console.log("로그아웃 성공");
       })
       .catch((error) => {
         // An error happened.
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -19,3 +20,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+export const db = getFirestore(app);

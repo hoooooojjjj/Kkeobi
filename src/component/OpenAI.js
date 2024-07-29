3ac162c925b937a76f1e348c70bda873009ba827
sono <rhj080471@gmail.com>
2024-07-29 17:59:43 +0900

    Feat : 사용자 실제 질문과 답변을 화면에 렌더링, 파이어베이스에 질문 답변 저장, 답변 로딩 애니메이션 구현

--- src/component/OpenAI.js
+++ src/component/OpenAI.js
@@ -2,13 +2,12 @@ import React, { useContext, useState } from "react";
 import axios from "axios";
 import { userObjContext } from "../App";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { storage } from "../firebase";
+import { db, storage } from "../firebase";
 import {
   Mains,
   ChatRoom,
   FirstChatWrap,
   FirstChat,
-  BillAnalysisBtn,
   QuestionBtn,
   MyChatWrap,
   MyChat,
@@ -16,7 +15,45 @@ import {
   KkeobiChat,
   MainLogo,
   BackBtn,
+  MessageInput,
+  SendMessageBtn,
+  MessageInputWrap,
+  StyledLabel,
+  PendingAnswer,
 } from "../page/main/MainStyle";
+import { doc, getDoc, setDoc, updateDoc, arrayUnion } from "firebase/firestore";
+
+const getChatLog = async (userObj) => {
+  const docRef = doc(db, "chatLog", userObj.uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    // docSnap.data() will be undefined in this case
+    console.log("No such document!");
+  }
+};
+
+const insertChatLog = async (userObj, content) => {
+  const chatLogRef = doc(db, "chatLog", userObj.uid);
+
+  try {
+    // 문서가 존재하는지 확인
+    const docSnap = await getDoc(chatLogRef);
+    if (docSnap.exists()) {
+      // 문서가 존재하면 업데이트하여 content를 배열에 추가
+      await updateDoc(chatLogRef, {
+        content: arrayUnion(content),
+      });
+    } else {
+      // 문서가 존재하지 않으면 새로운 문서를 생성하고 content를 배열로 저장
+      await setDoc(chatLogRef, { content: [content] });
+    }
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+};
 
 const ChatComponent = ({
   isChatRoomExpanded,
@@ -24,13 +61,13 @@ const ChatComponent = ({
   hidden,
 }) => {
   // 유저 정보
-  const { data } = useContext(userObjContext);
+  const { data: userObj } = useContext(userObjContext);
 
   // 사용자가 업로드한 이미지 파일
   const [imgFile, setimgFile] = useState();
 
-  // 채팅 시작 여부
-  const [isChatStarted, setIsChatStarted] = useState(false);
+  // 답변 왔는지 여부
+  const [isPending, setIsPending] = useState(false);
 
   // 사용자가 입력한 메세지
   const [content, setContent] = useState("");
@@ -38,30 +75,31 @@ const ChatComponent = ({
   // 대화 스레드 ID
   const [thread, setThread] = useState("");
 
-  // 사용자가 입력한 질문
-  const [question, setQuestion] = useState([]);
+  // 대화 기록
+  const [ChatLog, setChatLog] = useState([]);
 
-  // OpenAI로부터 받은 답변
-  const [answer, setAnswer] = useState([]);
-
-  // 이미지 업로드
-  const handleImageUpload = (e) => {
-    // 사용자가 업로드한 파일 가져오기
-    setimgFile(e.target.files[0]);
-  };
+  // // 이미지 업로드
+  // const handleImageUpload = (e) => {
+  //   // 사용자가 업로드한 파일 가져오기
+  //   console.log(e.target.files[0]);
+  //   setimgFile(e.target.files[0]);
+  // };
 
   // 대화 스레드 생성 시
-  const handleCreateThread = async () => {
+  const handleCreateThread = async (e) => {
+    setIsPending(true);
     // 참조 만들기
-    const storageRef = ref(storage, `billImg/${data.uid}/${imgFile.name}`);
+    const storageRef = ref(
+      storage,
+      `billImg/${userObj.uid}/${e.target.files[0].name}`
+    );
 
     // 파일 업로드
-    await uploadBytes(storageRef, imgFile);
+    await uploadBytes(storageRef, e.target.files[0]);
 
     // 파일 URL 가져오기
     const url = await getDownloadURL(storageRef);
 
-    setIsChatStarted(true);
     try {
       // '/chat' 엔드포인트로 POST 요청(이미지 URL 전송 후 대화 스레드 생성)
       const response = await axios.post(
@@ -73,11 +111,25 @@ const ChatComponent = ({
       );
       // '/chat' 요청에서 응답으로 받은 thread id 값을 상태에 저장
       setThread(response.data.thread);
-      // '/chat' 요청에서 받은 답변을 상태에 저장
-      setAnswer([...answer, response.data.response.answer]);
+
+      // '/chat' 요청에서 보낸 고지서 이미지 URL 질문, 답변을 상태에 저장
+      setChatLog([
+        ...ChatLog,
+        { question: url, answer: response.data.response.answer },
+      ]);
+
+      setIsPending(false);
+
+      insertChatLog(userObj, {
+        question: url,
+        answer: response.data.response.answer,
+      });
+
       // 콘솔에 thread id 출력
       console.log("Thread created! Thread id :", response.data.thread);
+
       // 콘솔에 '/chat' 요청에서 받은 답변 출력
+
       console.log("first response : ", response.data.response.answer);
     } catch (error) {
       // 에러 발생 시 콘솔에 에러 출력
@@ -87,8 +139,7 @@ const ChatComponent = ({
 
   // 메세지 전송 시
   const handleSendMessage = async () => {
-    // 질문 저장
-    setQuestion([...question, content]);
+    setIsPending(true);
     // 스레드 id가 생성된 스레드와 일치하는지 확인 용 콘솔
     console.log("thread:", thread);
     // 스레드가 없을 경우 콘솔에 출력
@@ -111,8 +162,19 @@ const ChatComponent = ({
         `sending response in ${thread} : `,
         response.data.response.answer
       );
-      // '/chat/message' 요청에서 받은 답변을 상태에 저장
-      setAnswer([...answer, response.data.response.answer]);
+
+      // '/chat/message' 요청에서 받은 질문, 답변을 상태에 저장
+      setChatLog([
+        ...ChatLog,
+        { question: content, answer: response.data.response.answer },
+      ]);
+
+      setIsPending(false);
+
+      insertChatLog(userObj, {
+        question: content,
+        answer: response.data.response.answer,
+      });
     } catch (error) {
       // 에러 발생 시 콘솔에 에러 출력
       console.error("Failed to send response : ", error);
@@ -121,33 +183,6 @@ const ChatComponent = ({
   };
 
   return (
-    // <div>
-    //   <h2>대화 시작하기</h2>
-    //   <input type="file" onChange={(e) => handleImageUpload(e)} />
-    //   {/* {imgFile ? <img src={imgFile} alt="img" style={{ width: "50px" }} /> : ""} */}
-    //   <button onClick={handleCreateThread}>대화 시작하기</button>
-    //   <h2>메세지 보내기</h2>
-    //   <input
-    //     type="text"
-    //     placeholder="Content"
-    //     value={content}
-    //     onChange={(e) => setContent(e.target.value)}
-    //   />
-    //   <button onClick={handleSendMessage}>보내기</button>
-
-    //   <h2>질문과 답변</h2>
-    //   {isChatStarted ? "채팅이 시작되었습니다. 잠시만 기다려주세요" : ""}
-    //   <p>답변 : {answer[0]}</p>
-    //   {question.map((q, index) => (
-    //     <div key={index}>
-    //       <p>질문: {q}</p>
-    //       <p>
-    //         답변:{" "}
-    //         {answer[index + 1] ? answer[index + 1] : "잠시간 기다려주세요"}
-    //       </p>
-    //     </div>
-    //   ))}
-    // </div>
     <Mains isChatRoomExpanded={isChatRoomExpanded}>
       <MainLogo src={process.env.PUBLIC_URL + `/assets/Logo.png`} />
       <BackBtn hidden={hidden} onClick={() => setIsChatRoomExpanded(false)}>
@@ -158,24 +193,62 @@ const ChatComponent = ({
           <FirstChat>안녕하세요, 꺼비입니다! 무엇을 도와드릴까요?</FirstChat>
         </FirstChatWrap>
         <div>
-          <BillAnalysisBtn onClick={() => setIsChatRoomExpanded(true)}>
-            내 고지서 분석
-          </BillAnalysisBtn>
+          <input
+            type="file"
+            id="billFile"
+            style={{ display: "none" }}
+            onChange={(e) => {
+              setIsChatRoomExpanded(true);
+              handleCreateThread(e);
+            }}
+          />
+          <StyledLabel htmlFor="billFile">내 고지서 분석</StyledLabel>
+
           <QuestionBtn onClick={() => setIsChatRoomExpanded(true)}>
             질문하기
           </QuestionBtn>
         </div>
-        <MyChatWrap>
-          <MyChat>기후환경요금이 무슨 뜻이야?</MyChat>
-        </MyChatWrap>
-        <KkeobiChatWrap>
-          <KkeobiChat>
-            기후환경요금에 대해 설명해드릴게요!☺️ 깨끗하고 안전한 에너지 제공에
-            소요되는 비용으로, "기후환경요금 단가*사용전력량"로 계산해요. 2024년
-            0월 00일 기준 기후환경요금 단가는 9원입니다.더 자세한 정보를 알고
-            싶다면 말해주세요!☺️
-          </KkeobiChat>
-        </KkeobiChatWrap>
+        {ChatLog.map((Chat, index) => (
+          <div>
+            <MyChatWrap>
+              <MyChat>{Chat.question}</MyChat>
+            </MyChatWrap>
+            <KkeobiChatWrap>
+              <KkeobiChat>{Chat.answer}</KkeobiChat>
+            </KkeobiChatWrap>
+          </div>
+        ))}
+        {isPending ? (
+          <KkeobiChatWrap>
+            <PendingAnswer answerLoaded={!isPending}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </PendingAnswer>
+          </KkeobiChatWrap>
+        ) : (
+          <></>
+        )}
+        {isChatRoomExpanded ? (
+          <MessageInputWrap>
+            <input
+              type="file"
+              id="file"
+              style={{ display: "none" }}
+              // onChange={handleImageUpload}
+            />
+            <StyledLabel htmlFor="file">+</StyledLabel>
+            {/* {imgFile ? <img src={imgFile} alt="img" style={{ width: "50px" }} /> : ""} */}
+            <MessageInput
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="메세지를 입력하세요"
+            ></MessageInput>
+            <SendMessageBtn onClick={handleSendMessage}>보내기</SendMessageBtn>
+          </MessageInputWrap>
+        ) : (
+          <></>
+        )}
       </ChatRoom>
     </Mains>
   );
--- src/page/main/MainStyle.js
+++ src/page/main/MainStyle.js
@@ -1,4 +1,6 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import React from "react";
 
 // 헤더
 
@@ -137,7 +139,122 @@ export const BillAnalysisBtn = styled.button({});
 // 메인 질문하기 버튼
 export const QuestionBtn = styled.button({});
 
+// 메시지 input wrap
+export const MessageInputWrap = styled.div({
+  position: "absolute",
+  bottom: 10,
+  width: "90%",
+  justifyContent: "center",
+  alignItems: "center",
+  display: "inline-flex",
+});
+
+export const StyledLabel = styled.label`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 10px;
+  background-color: #4753ff;
+  color: white;
+  border-radius: 20px;
+  font-size: 15px;
+  font-family: "Inter", sans-serif;
+  font-weight: 400;
+  cursor: pointer;
+  padding: 10px 20px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #3b48cc;
+  }
+
+  &:active {
+    background-color: #2f3a99;
+  }
+`;
+
+// 메시지 input
+export const MessageInput = styled.input({
+  width: "70%",
+  border: "1px solid #4753FF",
+  borderRadius: 20,
+  padding: 10,
+  fontSize: 15,
+  fontFamily: "Inter",
+  fontWeight: "400",
+  outline: "none",
+});
+// 메시지 보내기 버튼
+export const SendMessageBtn = styled.button({
+  width: "30%",
+  marginLeft: 10,
+  background: "#4753FF",
+  color: "white",
+  borderRadius: 20,
+  padding: 10,
+  fontSize: 15,
+  fontFamily: "Inter",
+  fontWeight: "400",
+  border: "none",
+  outline: "none",
+});
+
 // 구글 로그인 이미지
 export const GoogleLoginImg = styled.img({
   width: 200,
 });
+
+export const PendingAnswer = styled.div`
+  z-index: 100;
+  opacity: ${(props) => (props.answerLoaded ? 0 : 1)};
+  inset: 0;
+  width: 100%;
+  margin-right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > span {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background-color: gray;
+    border-radius: 50%;
+    animation: loading 1s infinite linear;
+  }
+  & > span:nth-of-type(0) {
+    margin: 5px;
+    animation-delay: 0s;
+    background-color: red;
+  }
+  & > span:nth-of-type(1) {
+    margin: 5px;
+    animation-delay: 0.2s;
+    background-color: dodgerblue;
+  }
+  & > span:nth-of-type(2) {
+    margin: 5px;
+    animation-delay: 0.4s;
+    background-color: greenyellow;
+  }
+  transition: opacity 0.1s ease-in-out;
+  ${(props) =>
+    !props.answerLoaded &&
+    `
+    opacity: 1;
+  `}
+
+  @keyframes loading {
+    0% {
+      opacity: 0;
+      transform: scale(0.5);
+    }
+    50% {
+      opacity: 1;
+      transform: scale(1.2);
+    }
+    100% {
+      opacity: 0;
+      transform: scale(0.5);
+    }
+  }
+`;
